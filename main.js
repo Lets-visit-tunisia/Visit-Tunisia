6a0a76f5e1e3918a7e18e29af595a1b96c8f0b73
zaatoutimohamed <mohamedzaatouti@gmail.com>
2021-06-24 16:49:09 +0200

    first push

---
+++ main.js
@@ -0,0 +1,33 @@
+var tunisiaCard = [
+{ name:"Djerba" ,
+ image: "./img/Djerba.jpg"
+},
+{ name:"Sousse" ,
+ image: "./img/Sousse.jpg"
+},
+{ name:"Hammamet" ,
+ image: "./img/Hammamet.jpg"
+},
+{ name:"Mahdia" ,
+ image: "./img/Sousse.jpg"
+},
+{ name:"Tataouine" ,
+ image: "./img/Sousse.jpg"
+},
+{ name:"tozeur" ,
+ image: "./img/Sousse.jpg"
+},
+{ name:"ElJem" ,
+ image: "./img/ElJem.jpg"
+},
+
+];
+//added by Ali
+$("#btn").on("click",function(){
+$("img").attr("src",tunisiaCard["click"].image)}
+	
+
+ /*function onclick(tunisiaCard){
+ 	return map()
+ }
+*/
